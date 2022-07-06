9ffb273886eaa128b3b18af4c0e979514bdca673
yongzhi.liu <yongzhi1.liu@gmail.com>
2022-07-06 23:42:28 +0800

    codesandbox

--- pages/detail.tsx
+++ pages/detail.tsx
@@ -1,10 +1,25 @@
 import Title from '../components/title'
+import {
+    SandpackProvider,
+    SandpackLayout,
+    SandpackCodeEditor,
+    SandpackPreview,
+    SandpackReactDevTools,
+    SandpackTranspiledCode,
+  } from "@codesandbox/sandpack-react"
 export default () => {
-
+    
     return (
         <>
             <Title title={'JavaScript-继承'} />
             继承
+            <SandpackProvider template="react">
+                <SandpackLayout>
+                <SandpackCodeEditor />
+                <SandpackPreview />
+                <SandpackReactDevTools />
+                </SandpackLayout>
+            </SandpackProvider>
         </>
     )
 }
