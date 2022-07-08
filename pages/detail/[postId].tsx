b1df657c707cb18bcbb14def47da021b8b40134d
yongzhi.liu <yongzhi1.liu@gmail.com>
2022-07-08 23:18:55 +0800

    codesandbox

--- pages/detail/[postId].tsx
+++ pages/detail/[postId].tsx
@@ -1,26 +1,39 @@
 import Title from '../../components/title'
 import { useRouter } from 'next/router'
-import {
-    SandpackProvider,
-    SandpackLayout,
-    SandpackCodeEditor,
-    SandpackPreview,
-    SandpackReactDevTools,
-  } from "@codesandbox/sandpack-react"
+import { Sandpack } from "@codesandbox/sandpack-react"
+import { nightOwl } from "@codesandbox/sandpack-themes";
+import {useEffect, useState} from "react";
 export default () => {
     const router = useRouter()
     const { postId } = router.query
+    const [code, setCode] = useState<string>('')
+    const [css, setCss] = useState<string>('')
+    useEffect(() => {
+        setCss(`div {
+            color: red;
+        }`)
+        setCode(`
+        import './style.css'
+        export default function App() {
+          return <h1>Hello Sandpack</h1>
+        }`)
+    })
     return (
         <>
             <Title title={'JavaScript-继承'} />
             继承{postId}
-            <SandpackProvider template="react">
-                <SandpackLayout>
-                <SandpackCodeEditor />
-                <SandpackPreview />
-                <SandpackReactDevTools />
-                </SandpackLayout>
-            </SandpackProvider>
+            <Sandpack
+                theme={nightOwl}
+                template="react"
+                files={{
+                    "/App.js": code,
+                    "/style.css": css
+                }}
+                options={{
+                    showNavigator: true,
+                    autorun: false
+                }}
+            />
         </>
     )
 }
